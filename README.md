9931ad06611c847dc4cf11f506b8ceb207029a6a
sam0622 <sgreenfield517@gmail.com>
2023-10-23 15:36:05 -0400

    yeah

---
+++ README.md
@@ -0,0 +1,2 @@
+# Coin Dash
+ a test repo that we can use for the Coin Dash project VGD
---
+++ player.gd
@@ -0,0 +1,17 @@
+extends Area2D
+
+@export var speed = 350
+var velocity = Vector2.ZERO
+var screensize = Vector2(480, 720)
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	velocity = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	position += velocity * speed * delta
+
