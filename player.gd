1987ad76a668dfcd0214ca224e5a06cb79dd0430
sam0622 <sgreenfield517@gmail.com>
2023-10-23 15:45:11 -0400

    Update player.gd

--- player.gd
+++ player.gd
@@ -16,3 +16,10 @@ func _process(delta):
 	position += velocity * speed * delta
 	position.x = clamp(position.x, 0, screensize.x)
 	position.y = clamp(position.y, 0, screensize.y)
+	
+	if velocity.length() > 0:
+		$AnimatedSprite2D.animation = "run"
+	else:
+		$AnimatedSprite2D.animation = "idle"
+	if velocity.x != 0:
+		$AnimatedSprite2D.flip_h = velocity.x < 0
