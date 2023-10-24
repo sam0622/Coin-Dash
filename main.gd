ed510019fa7d4223443a34733b1b74b477e3e887
sam0622 <sgreenfield517@gmail.com>
2023-10-24 10:03:27 -0400

    added main scene

---
+++ main.gd
@@ -0,0 +1,15 @@
+extends Node
+
+@export var coin_scene : PackedScene
+@export var playtime = 30
+
+var level = 1
+var score = 0
+var time_left = 0
+var screensize = Vector2.ZERO
+var playing = false
+
+func _ready():
+	screensize = get_viewport().get_visible_rect().size
+	$Player.screensize = screensize
+	$Player.hide()
